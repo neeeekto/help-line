71f7f32c25fa5cd566a3df48562ee83d74ccf767
yymoroz3 <yymoroz3@yandex.ru>
2022-12-26 09:56:25 +0300

    ui v2

--- src/UI/apps/admin/vite.config.ts
+++ src/UI/apps/admin/vite.config.ts
@@ -1,13 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import ViteTsConfigPathsPlugin from 'vite-tsconfig-paths';
+import viteTsConfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
   plugins: [
     react(),
-    ViteTsConfigPathsPlugin({
+    viteTsConfigPaths({
       root: '../../',
-      projects: ['tsconfig.base.json'],
     }),
   ],
 });
